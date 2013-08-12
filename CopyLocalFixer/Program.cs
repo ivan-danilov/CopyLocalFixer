50f98fcde6488e3c9b18da145f84b696cf1e9bf2
ivan.danilov-econ <PROD\ivan.danilov-econ>
2013-08-12 16:38:38 +0300

    Added /rewrite and /restore keys to be able to restore original csproj state after build finished or failed.

--- CopyLocalFixer/Program.cs
+++ CopyLocalFixer/Program.cs
@@ -8,57 +8,88 @@ namespace CopyLocalFixer
 {
     internal class Program
     {
+        private static void WriteUsage()
+        {
+            Console.WriteLine("Usage: CopyLocalFixer {/rewrite | /restore} <Path-to-SLN-file>");
+            Console.WriteLine();
+            Console.WriteLine("CopyLocalFixer analyzes solution and re-writes csproj files so that each external");
+            Console.WriteLine("assembly referenced with CopyLocal (or Private which is the same) not more than once.");
+            Console.WriteLine("It is needed because otherwise during parallel build copying of same file happens");
+            Console.WriteLine("twice at times and results in an error about not accessible file for one of them.");
+        }
+
         private static void Main(string[] args)
         {
-            if (args.Length != 1 || args[0] == "-h" || args[0] == "/?" || args[0] == "/h")
+            if (args.Length != 2 || args[0] == "-h" || args[0] == "/?" || args[0] == "/h" ||
+                args[0] != "/rewrite" && args[0] != "/restore")
             {
-                Console.WriteLine("CopyLocalFixer takes path to a solution file as the single argument");
-                Console.WriteLine();
-                Console.WriteLine("CopyLocalFixer analyzes solution and re-writes csproj files so that each external");
-                Console.WriteLine("assembly referenced with CopyLocal (or Private which is the same) not more than once.");
-                Console.WriteLine("It is needed because otherwise during parallel build copying of same file happens");
-                Console.WriteLine("twice at times and results in an error about not accessible file for one of them.");
+                WriteUsage();
                 return;
             }
+            bool rewrite = args[0] == "/rewrite";
 
-            var solutionPath = args[0];
+            var solutionPath = args[1];
             var sln = new SlnParser().Parse(new StreamReader(solutionPath));
 
             var assemblies = new HashSet<string>();
             foreach (var csprojPath in sln.Projects.Select(p => p.Path))
             {
                 var fullCsprojPath = Path.Combine(Path.GetDirectoryName(solutionPath), csprojPath);
-                StripReadonlyIfSet(fullCsprojPath);
-                var doc = XDocument.Load(fullCsprojPath);
-                var items = from itemGroupElement in doc.Root.Elements()
-                    where itemGroupElement.Name.LocalName == "ItemGroup"
-                    from item in itemGroupElement.Elements()
-                    select item;
+                if (rewrite)
+                    Rewrite(fullCsprojPath, assemblies);
+                else
+                    Restore(fullCsprojPath);
+            }
+        }
 
-                var refElements = from item in items
-                    where item.Name.LocalName == "Reference"
-                    select item;
+        private static void Rewrite(string fullCsprojPath, HashSet<string> assemblies)
+        {
+            bool wasChanged = false;
+            var doc = XDocument.Load(fullCsprojPath);
+            var items = from itemGroupElement in doc.Root.Elements()
+                where itemGroupElement.Name.LocalName == "ItemGroup"
+                from item in itemGroupElement.Elements()
+                select item;
 
-                foreach (var refElement in refElements)
+            var refElements = from item in items
+                where item.Name.LocalName == "Reference"
+                select item;
+
+            foreach (var refElement in refElements)
+            {
+                string assemblyName = refElement.Attributes().Single(attr => attr.Name.LocalName == "Include").Value;
+                string filename = assemblyName.Split(',')[0];
+                var isPrivateElement = refElement.Elements().FirstOrDefault(e => e.Name.LocalName == "Private");
+                bool isPrivate = isPrivateElement == null || !String.Equals(isPrivateElement.Value, "false", StringComparison.InvariantCultureIgnoreCase);
+                if (!isPrivate) continue;
+                bool firstTime = assemblies.Add(filename);
+                if (firstTime) continue;
+
+                wasChanged = true;
+                if (isPrivateElement == null)
                 {
-                    string assemblyName = refElement.Attributes().Single(attr => attr.Name.LocalName == "Include").Value;
-                    string filename = assemblyName.Split(',')[0];
-                    var isPrivateElement = refElement.Elements().FirstOrDefault(e => e.Name.LocalName == "Private");
-                    bool isPrivate = isPrivateElement == null || !String.Equals(isPrivateElement.Value, "false", StringComparison.InvariantCultureIgnoreCase);
-                    if (!isPrivate) continue;
-                    bool firstTime = assemblies.Add(filename);
-                    if (firstTime) continue;
-                    if (isPrivateElement == null)
-                    {
-                        isPrivateElement = new XElement("Private", "False");
-                        refElement.Add(isPrivateElement);
-                    }
-                    else
-                    {
-                        isPrivateElement.Value = "False";
-                    }
+                    isPrivateElement = new XElement("Private", "False");
+                    refElement.Add(isPrivateElement);
                 }
+                else
+                {
+                    isPrivateElement.Value = "False";
+                }
+            }
+            if (wasChanged)
+            {
+                StripReadonlyIfSet(fullCsprojPath);
+                File.Move(fullCsprojPath, fullCsprojPath + ".backup");
                 doc.Save(fullCsprojPath, SaveOptions.None);
+            }
+        }
+
+        private static void Restore(string fullCsprojPath)
+        {
+            if (File.Exists(fullCsprojPath + ".backup"))
+            {
+                File.Delete(fullCsprojPath);
+                File.Move(fullCsprojPath + ".backup", fullCsprojPath);
             }
         }
 
